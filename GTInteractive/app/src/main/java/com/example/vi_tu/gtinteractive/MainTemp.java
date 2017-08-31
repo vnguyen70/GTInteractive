38a2cec71a20b949f7cb2abf97995cb71518b223
vnguyen70 <nguyen.vi@gatech.edu>
2017-08-31 18:53:19 -0400

    Vi - More Setup Stuff
    Internal Map Screen button doesn't work  rn. The other 2 buttons link to their respective pages.

---
+++ GTInteractive/app/src/main/java/com/example/vi_tu/gtinteractive/MainTemp.java
@@ -0,0 +1,41 @@
+package com.example.vi_tu.gtinteractive;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainTemp extends AppCompatActivity {
+    Button mapButton;
+    Button buildingDetailButton;
+    Button internalMapButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_temp);
+
+        mapButton = (Button) findViewById(R.id.mapButton);
+        buildingDetailButton = (Button) findViewById(R.id.buildingDetailButton);
+        internalMapButton = (Button) findViewById(R.id.internalMapButton);
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mapIntent = new Intent(MainTemp.this, MainMapActivity.class);
+                startActivity(mapIntent);
+            }
+        });
+
+        buildingDetailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent buildingDetailIntent = new Intent(MainTemp.this, buildingDetail.class);
+                startActivity(buildingDetailIntent);
+            }
+        });
+    }
+
+
+}
